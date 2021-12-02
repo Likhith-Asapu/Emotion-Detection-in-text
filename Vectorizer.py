dd82751be3af778cef873b5e31c264a774a8216b
Rutts07 <rahothvarman07@gmail.com>
2021-12-02 21:36:05 +0530

    Fix a bug in Vectorizer.py

--- Vectorizer.py
+++ Vectorizer.py
@@ -24,7 +24,7 @@ tweettextonly = [
 stopwords = nltk.corpus.stopwords.words('english')
 
 # unigrams
-cv = CountVectorizer(ngram_range=(1,1), stop_words = stopwords, max_features = 10)
+cv = CountVectorizer(ngram_range=(1,1), stop_words = stopwords, max_features = 50)
 sparsearray = cv.fit_transform(tweettextonly)
 vocab = cv.vocabulary_
 count_values = sparsearray.toarray().sum(axis=0)
@@ -35,7 +35,7 @@ for ug_count, ug_text in sorted([(count_values[i],k) for k,i in vocab.items()], 
 print(unigrams)
 
 #bigrams
-cv = CountVectorizer(ngram_range=(2,2), stop_words = stopwords, max_features = 10)
+cv = CountVectorizer(ngram_range=(2,2), stop_words = stopwords, max_features = 50)
 sparsearray = cv.fit_transform(tweettextonly)
 vocab = cv.vocabulary_
 count_values = sparsearray.toarray().sum(axis=0)
@@ -46,7 +46,7 @@ for bg_count, bg_text in sorted([(count_values[i],k) for k,i in vocab.items()], 
 print(bigrams)
 
 #trigrams
-cv = CountVectorizer(ngram_range=(3,3), stop_words = stopwords, max_features = 10)
+cv = CountVectorizer(ngram_range=(3,3), stop_words = stopwords, max_features = 50)
 sparsearray = cv.fit_transform(tweettextonly)
 vocab = cv.vocabulary_
 count_values = sparsearray.toarray().sum(axis=0)
@@ -66,25 +66,39 @@ for tweet in tweettextonly:
     bi = []
     tri = []
     
+    
+    tokens = tweet.split(" ")
+    
+    # calculate the frequency of uni-gram
     for ug in unigrams:
+        count = 0
         if ug in tweet:
-            uni.append(1)
-        else :
-            uni.append(0)
+            for token in tokens :
+                if (ug == token):
+                    count = count + 1
+        uni.append(count)
     vec.append(uni)
     
+    # calculate the frequency of bi-gram
     for bg in bigrams:
+        count = 0
         if bg in tweet:
-            bi.append(1)
-        else :
-            bi.append(0)
+            for i in range(0, len(tokens)-1):
+                bigram = tokens[i] + " " + tokens[i+1]
+                if (bg == bigram):
+                    count = count + 1
+        bi.append(count)
     vec.append(bi)
     
+    # calculate the frequency of tri-gram
     for tg in trigrams:
+        count = 0
         if tg in tweet:
-            tri.append(1)
-        else :
-            tri.append(0)
+            for i in range(0, len(tokens)-2):
+                trigram = tokens[i] + " " + tokens[i+1] + " " + tokens[i+2]
+                if (tg == trigram):
+                    count = count + 1
+        tri.append(count)
     vec.append(tri)
     
     result.append(vec)
